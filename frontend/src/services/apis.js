4cdbb2aa5fd79a869fd480f2b7d4d8190bf6838b
chandelshivam4256 <chandelshivam4256@gmail.com>
2025-10-29 22:56:07 +0530

    changes in api.js and some .env settings

--- frontend/src/services/apis.js
+++ frontend/src/services/apis.js
@@ -1,5 +1,5 @@
 //const BASE_URL = import.meta.env.VITE_APP_BASE_URL;// backend url
-const BASE_URL = "http://localhost:3000/api/v1";
+const BASE_URL = import.meta.env.VITE_APP_BACKEND_BASE_URL;
 // const BASE_URL = "https://ed-tech-platform-3fgs.onrender.com/api/v1";
 
 // AUTH ENDPOINTS
